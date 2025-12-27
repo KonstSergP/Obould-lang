4468beb58f251c9d1c803c9b084f762399d24caf
KonstSergP <petrovkostyasergeevich@yandex.ru>
2025-12-27 23:26:16 +0300

    Добавил возвращаемое выражение в ProcedureDeclaration

--- src/ast/ProcedureDeclaration.h
+++ src/ast/ProcedureDeclaration.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include "ASTNode.h"
+#include "Expression.h"
 #include "ProcedureParameter.h"
 #include "StatementsBlock.h"
 #include "Type.h"
@@ -13,12 +14,13 @@ class ProcedureDeclaration : public ASTNode
 public:
     ProcedureDeclaration(const std::string& name, bool isExported,
                          std::vector<std::unique_ptr<ProcedureParameter>> parameters, std::unique_ptr<Type> returnType,
-                         std::unique_ptr<StatementsBlock> body)
+                         std::unique_ptr<StatementsBlock> body, std::unique_ptr<Expression> returnExpression)
         : name(name),
           isExported(isExported),
           parameters(std::move(parameters)),
           returnType(std::move(returnType)),
-          body(std::move(body)) {}
+          body(std::move(body)),
+          returnExpression(std::move(returnExpression)) {}
 
     void accept(ASTVisitor& v) override;
 
@@ -28,4 +30,5 @@ public:
     std::vector<std::unique_ptr<ProcedureParameter>> parameters;
     std::unique_ptr<Type> returnType;
     std::unique_ptr<StatementsBlock> body;
+    std::unique_ptr<Expression> returnExpression;
 };
