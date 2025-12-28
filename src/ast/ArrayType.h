26ff77a122d1d9511ef735f6dba32598a7a51460
KonstSergP <petrovkostyasergeevich@yandex.ru>
2025-12-28 21:18:14 +0300

    Длина ArrayType заменена на выражение

--- src/ast/ArrayType.h
+++ src/ast/ArrayType.h
@@ -1,18 +1,20 @@
 #pragma once
 #include <memory>
+
+#include "Expression.h"
 #include "Type.h"
 
 
 class ArrayType : public Type
 {
 public:
-    ArrayType(std::unique_ptr<Type> elementType, int length)
+    ArrayType(std::unique_ptr<Type> elementType, std::unique_ptr<Expression> length)
         : elementType(std::move(elementType)),
-          length(length) {}
+          length(std::move(length)) {}
 
     void accept(ASTVisitor& v) override;
 
 
     std::unique_ptr<Type> elementType;
-    int length;
+    std::unique_ptr<Expression> length;
 };
