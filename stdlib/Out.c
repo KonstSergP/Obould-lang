d7d68352089fc139088eae8c00cbdb4b2562d229
KonstSergP <petrovkostyasergeevich@yandex.ru>
2026-03-17 16:08:51 +0300

    Вывод чисел фибоначчи

--- stdlib/Out.c
+++ stdlib/Out.c
@@ -1,28 +1,28 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void ob_3Out_WriteInt(int64_t x);
-void ob_3Out_WriteLn(void);
-void ob_3Out_init(void);
-void ob_Out(void);
+void WriteInt(int64_t x) __asm__("ob_3Out_WriteInt");
+void WriteLn(void) __asm__("ob_3Out_WriteLn");
+void init(void) __asm__("ob_3Out_init");
+void Out(void) __asm__("ob_Out");
 
-void ob_3Out_WriteInt(int64_t x)
+void WriteInt(int64_t x)
 {
     printf("%lld", (long long)x);
     fflush(stdout);
 }
 
-void ob_3Out_WriteLn(void)
+void WriteLn(void)
 {
     putchar('\n');
     fflush(stdout);
 }
 
-void ob_3Out_init(void)
+void init(void)
 {
 }
 
-void ob_Out(void)
+void Out(void)
 {
-    ob_3Out_init();
+    init();
 }
